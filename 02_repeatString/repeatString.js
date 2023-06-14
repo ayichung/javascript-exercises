0084ec3cd99f2ee061cf6e327152728b08e6365f
ayichung <121764547+ayichung@users.noreply.github.com>
2023-06-14 14:14:38 -0400

    Complete exercise 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    rep_str = "";
+    for(i=0; i<num; i++) {
+        rep_str += str
+    }
+    return rep_str;
 };
 
 // Do not edit below this line
