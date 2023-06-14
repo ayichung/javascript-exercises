d104d6d4fcce3093c2329ac04422564a682d1e80
ayichung <121764547+ayichung@users.noreply.github.com>
2023-06-14 15:06:22 -0400

    Complete exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0];
+    console.log(arr)
+    for (i=1; i<arguments.length; i++) {
+        let id = arr.indexOf(arguments[i]);
+        if (id > -1) {
+            arr.splice(id, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
