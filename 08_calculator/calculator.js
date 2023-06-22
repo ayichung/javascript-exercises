10ae7340455c0e87c2fbeaa525b99ec2a28fbebf
ayichung <121764547+ayichung@users.noreply.github.com>
2023-06-22 14:58:32 -0400

    Complete exercise 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (const num of arr) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  prod = 1;
+  for (const num of args) {
+    prod = prod * num;
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  prod = 1
+	for (i=1; i<=num; i++) {
+    prod = prod * i;
+  }
+  return prod;
 };
 
 // Do not edit below this line
