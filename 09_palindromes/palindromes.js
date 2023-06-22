acd58c7edd65a82a1b238c81c38d5fa9633aa2ff
ayichung <121764547+ayichung@users.noreply.github.com>
2023-06-22 16:03:42 -0400

    Complete exercise 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const processedString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const reversedString = processedString.split("").reverse().join("");
+    return processedString == reversedString;
 };
 
 // Do not edit below this line
