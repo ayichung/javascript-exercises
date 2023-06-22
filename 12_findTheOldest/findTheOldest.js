fcceb8042edf64777fc4c3e63831c6a7b5d14a79
ayichung <121764547+ayichung@users.noreply.github.com>
2023-06-22 16:50:08 -0400

    Complete exercise 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldest = arr.reduce((oldest, cur) => {
+        const date = new Date();
+        if (cur.yearOfDeath == undefined) { curAge = date.getFullYear() - cur.yearOfBirth; }
+        else { curAge = cur.yearOfDeath - cur.yearOfBirth; }
+        if (oldest.yearOfDeath == undefined) { oldestAge = date.getFullYear() - oldest.yearOfBirth; }
+        else { oldestAge = oldest.yearOfDeath - oldest.yearOfBirth; }
+        if (curAge > oldestAge) {
+            return cur;
+        }
+        return oldest;
+    });
+    return oldest;
 };
 
 // Do not edit below this line
